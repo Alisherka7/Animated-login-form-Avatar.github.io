989794b337d677c264b6a97a596668cecc63d05f
Alisher Tog'ayev <38793933+Alisherka7@users.noreply.github.com>
2018-12-21 08:12:30 -0800

    Add files via upload

---
+++ Animated author/scss/style.scss
@@ -0,0 +1,87 @@
+/* colors */
+$darkBlue: #217093;
+$medBlue: #4eb8dd;
+$lightBlue: #ddf1fa;
+$inputBG: #f3fafd;
+
+html {width: 100%; height: 100%;}
+body {
+	background-color: #eff3f4; position: relative; width: 100%; height: 100%;
+	font-size: 16px; font-family: 'Source Sans Pro', sans-serif; font-weight: 400;
+	-webkit-font-smoothing: antialiased;
+}
+form {
+	position: absolute; top: 50%; left: 50%; transform: translate(-50%,-50%);
+	display: block; width: 100%; max-width: 400px; background-color: #FFF;
+	margin: 0; padding: 2.25em; box-sizing: border-box; border: solid 1px #DDD; border-radius: .5em;
+	font-family: 'Source Sans Pro', sans-serif;
+	.svgContainer {
+		position: relative; width: 200px; height: 200px; margin: 0 auto 1em;
+		border-radius: 50%; background: none; border: solid 2.5px #3A5E77; overflow: hidden;
+		pointer-events: none;
+		div {
+			position: relative; width: 100%; height: 0; overflow: hidden;
+			padding-bottom: 100%;
+		}
+		.mySVG {
+			position: absolute; left: 0; top: 0; width: 100%; height: 100%;
+			pointer-events: none;
+		}
+	}
+	.inputGroup {
+		margin: 0 0 2em; padding: 0; position: relative;
+		&:last-of-type {
+			margin-bottom: 0;
+		}
+	}
+	label {
+		margin: 0 0 12px; display: block;
+		font-size: 1.25em; color: #217093; font-weight: 700; font-family: inherit;
+	}
+	input[type='email'], input[type="text"], input[type='password'] {
+		display: block; margin: 0; padding: 0 1em 0;
+		background-color: $inputBG; border: solid 2px $darkBlue; border-radius: 4px; -webkit-appearance: none;
+		box-sizing: border-box;
+		width: 100%; height: 65px;
+		font-size: 1.55em; color: #353538; font-weight: 600; font-family: inherit;
+		transition: box-shadow .2s linear, border-color .25s ease-out;
+		&:focus {
+			outline: none;
+			box-shadow: 0px 2px 10px rgba(0,0,0,.1);
+			border: solid 2px #4eb8dd;
+		}
+	}
+	input[type='email'], input[type="text"] {
+		padding: 14px 1em 0px;
+	}
+	button {
+		display: block; margin: 0; padding: .65em 1em 1em;
+		background-color: $medBlue; border: none; border-radius: 4px;
+		box-sizing: border-box; box-shadow: none;
+		width: 100%; height: 65px;
+		font-size: 1.55em; color: #FFF; font-weight: 600; font-family: inherit;
+		transition: background-color .2s ease-out;
+		&:hover, &:active {
+			background-color: $darkBlue;
+		}
+	}
+
+	.inputGroup1 {
+		.helper {
+			position: absolute; z-index: 1;
+			font-family: inherit;
+		}
+		.helper1 {
+			top: 0; left: 0;
+			transform: translate(1.4em, 2.6em) scale(1); transform-origin: 0 0;
+			color: $darkBlue; font-size: 1.25em; font-weight: 400; opacity: .65;
+			pointer-events: none;
+			transition: transform .2s ease-out, opacity .2s linear;
+		}
+		
+		&.focusWithText .helper {
+		/*input[type='email']:focus + .helper {*/
+			transform: translate(1.4em, 2.0em) scale(.65); opacity: 1;
+		}
+	}
+}
